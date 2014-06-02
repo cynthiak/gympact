5bc1b88a524beda458a8f45bdffb67ce7e751084
Cynthia Koo <cyn.koo@gmail.com>
2014-06-02 00:00:38 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gympact::Application.config.secret_token = '781cfb5ecbd150002bdb0d8de4d1d1d77cfab6d867215d7c668d845ca167fddf84ad5482a321689051727dbb23d0abd7b19776b66e122e8ab06c008f0fc7eecb'
