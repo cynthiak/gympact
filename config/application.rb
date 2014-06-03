90cb0d1e973ae1a2b23195f6d842b72fc08ca007
Cynthia Koo <cyn.koo@gmail.com>
2014-06-03 00:53:09 -0400

    Fixing inaccessible db error when pushing to Heroku

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Added 6.3.14 per https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -61,5 +60,8 @@ module Gympact
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added 6.3.14 per https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
