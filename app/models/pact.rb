10ae08b150f7123bcdc087e9a8566cb1465dfa91
Cynthia Koo <cyn.koo@gmail.com>
2014-06-08 16:52:23 -0400

    FIXED Admin Pact plug - something was wrong with using has_many, :through

--- app/models/pact.rb
+++ app/models/pact.rb
@@ -2,12 +2,12 @@ class Pact < ActiveRecord::Base
 	has_many :users
 
 	has_many :chats
-	has_many :messages, through: :chats
+	# has_many :messages, through: :chats
 
 	has_many :penalties
 
 	has_many :weeks
-	has_many :workouts, through: :weeks
+	# has_many :workouts, through: :weeks
 
   attr_accessible :end_date, :is_active, :pact_name, :start_date
 end
