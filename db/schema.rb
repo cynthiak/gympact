66d81384106bff56a3f214e582b56e04a31b60e0
Cynthia Koo <cyn.koo@gmail.com>
2014-06-11 08:41:12 -0400

    Added date, time to workouts and photo ID to workouts // HTML skeleton for photo editor and chat page done // Week template started

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140608155133) do
+ActiveRecord::Schema.define(:version => 20140609225011) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -156,8 +156,12 @@ ActiveRecord::Schema.define(:version => 20140608155133) do
     t.datetime "updated_at",          :null => false
     t.integer  "user_id"
     t.integer  "week_id"
+    t.date     "date"
+    t.time     "time"
+    t.integer  "photo_id"
   end
 
+  add_index "workouts", ["photo_id"], :name => "index_photo_id"
   add_index "workouts", ["user_id"], :name => "index_user_id_3"
   add_index "workouts", ["week_id"], :name => "index_week_id"
 
