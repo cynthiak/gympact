ba57c06f6cc715c095769588870f2488f9dab3ec
Cynthia Koo <cyn.koo@gmail.com>
2014-06-26 17:58:35 -0400

    Removed migration file that was causing heroku error

--- db/migrate/20140613224342_remove_chat_id_from_messages.rb
+++
@@ -1,9 +0,0 @@
-class RemoveChatIdFromMessages < ActiveRecord::Migration
-  def up
-    remove_column :messages, :chat_id
-  end
-
-  def down
-    add_column :messages, :chat_id, :integer
-  end
-end
